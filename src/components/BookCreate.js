f0c9eeb3f045b89a52aff40e2ee4a3eb11d1aecb
Etson <etsond@ufl.edu>
2023-01-13 16:56:31 -0500

    adding an empty string after user enter book title

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,34 @@
+// input elementnneds a value prop and the onChange prop
+import { useState } from "react";
+
+
 function BookCreate({ onCreate }) {
-    return <div>BookCreate</div>
+    const [title, setTitle] = useState('');
+
+    // keeps track of any change the user input
+    const handleChange = (event) => {
+        // to look at what the usre type and then update the title state
+        setTitle(event.target.value);
+
+    };
+
+    const handleSubmit = (event) => {
+        // form element watns to submit by realoding so preventing
+        event.preventDefault();
+        // calling on create to pass the users input
+        onCreate(title);
+        // after the user enter the title then to empty the input
+        setTitle('');
+    }
+    return <div>
+        {/* calling the handler when the user submit the form */}
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            {/* o */}
+            <input value={title} onChange={handleChange}/>
+            <button>Submit</button>
+        </form>
+    </div>
 }
 
 export default BookCreate;
